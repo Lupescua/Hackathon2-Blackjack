5d890f60a8ab64e9a9a5e1884cc50c41a85662ab
Unknown <zerayah@gmail.com>
2018-02-02 10:06:07 +0100

    comment

---
+++ js/t.js
@@ -0,0 +1,21 @@
+// Create and shuffle a deck of cards - we already have this
+
+
+
+
+
+
+
+
+
+// Create two BlackjackHands, userHand and dealerHand
+// Deal two cards into each hand
+
+
+// Check if dealer has blackjack (if so, game ends)
+// Check if user has blackjack (if so, game ends)
+
+
+// User draws cards (if user goes over 21, game ends)
+// Dealer draws cards
+// Check for winner
