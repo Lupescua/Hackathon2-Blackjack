7ed117d23c8c35712a75e50f13d2a3f83b914da1
Unknown <lupescua@gmail.com>
2018-02-02 11:01:45 +0100

    Created a mock Dealer & Player function

--- js/t.js
+++ js/t.js
@@ -6,36 +6,46 @@
 
 // Create two BlackjackHands, userHand and dealerHand -
 
-function Dealer(color, name, x, y, mouth, facing) {
-	this.color = color;
-	this.name = name;
-	this.x = x;
-	this.y = y;
-	this.facing = facing;
-	this.mouth = mouth;
+function Dealer(card) {
+
+	this.card[{'suit': suits[s]},
+    {'value': values[v]},
+    {'reversed': false}],
+
+
+    this. sum: card[{1.value}]+card[{2.value}]+card[{3.value}],
 
 	this.info = function() {
-		return this.color + ' ' + this.name;
+        return this.sum;
     }
 
-function Player1(color, name, x, y, mouth, facing) {
-    this.color = color;
-    this.name = name;
-    this.x = x;
-    this.y = y;
-    this.facing = facing;
-    this.mouth = mouth;
+function Player1(card) {
+	this.card[{'suit': suits[s]},
+    {'value': values[v]},
+    {'reversed': false}],
+this. sum: card[{1.value}]+card[{2.value}]+card[{3.value}],
 
-    this.info = function() {
-        return this.color + ' ' + this.name;
+	this.info = function() {
+        return this.sum;
     }
 
 // Deal two cards into each hand
 
+// my comment: Function which targets a user, and gives him 1 card.
+
+function Deal(target, card){
+    deck
+// append the users deck with his card
+
+}
 
 // Check if dealer has blackjack (if so, game ends)
 // Check if user has blackjack (if so, game ends)
 
+// my comment: Create a check value of user deck.
+function user_deck(target){
+
+}
 
 // User draws cards (if user goes over 21, game ends)
 // Dealer draws cards
